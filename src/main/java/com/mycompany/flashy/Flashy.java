5322d8883ed80d783970fab7e9fdf80734305b8e
BaigHasan <baig.hasan@outlook.com>
2024-01-10 15:15:58 -0500

    Initial commit

---
+++ src/main/java/com/mycompany/flashy/Flashy.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.flashy;
+
+/**
+ *
+ * @author baigh
+ */
+public class Flashy {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
