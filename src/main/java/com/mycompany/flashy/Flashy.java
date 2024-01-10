173f0534d7db52f4c29d59617502dac99045db8e
BaigHasan <baig.hasan@outlook.com>
2024-01-10 15:26:17 -0500

    Commit to reset upstream

--- src/main/java/com/mycompany/flashy/Flashy.java
+++ src/main/java/com/mycompany/flashy/Flashy.java
@@ -11,6 +11,6 @@ package com.mycompany.flashy;
 public class Flashy {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Flashy!");
     }
 }
