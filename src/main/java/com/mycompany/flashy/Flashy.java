82fc241de5ecc5c2cd08bda20329c4bd49d9d3cd
Arpan Roy <arpan.roy_46@outlook.com>
2024-01-10 20:54:43 -0500

    hi

--- src/main/java/com/mycompany/flashy/Flashy.java
+++ src/main/java/com/mycompany/flashy/Flashy.java
@@ -12,6 +12,6 @@ public class Flashy {
 
     public static void main(String[] args) {
         System.out.println("Flashy!");
-        System.out.println("Soham loves Hasan!");
+        System.out.println("Soham loves Hasn!");
     }
 }
